0f6b686fa10a5c1178ecf61457dcfa7aba335400
iamBijoyKar <bijoypoco12345@gmail.com>
2022-08-16 22:39:23 +0530

    extra line breaks are deleted

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 from random import randint
 from rich.console import Console
-
-
 import time
 console=Console()
 total_point,upper_limit,lose,level,chance=0,5,False,1,5
@@ -18,22 +16,18 @@ while True:
         with console.status ("[bold green]Generatin...") as status:
             for i in range(1,11):
                 time.sleep(.1)
-        
         console.print(f'Random Generated Value is {rand_val}',style='bold green ')
         time.sleep(.5)
-
         if(str(rand_val) == guess_number):
             level_point+=100
             total_point+=level_point
             console.print(f' ** You Won ** \n Level {level} Completed',style='blink blue')
             time.sleep(.5)
-
             console.print(f'Total Point is {total_point} and Level Point is {level_point}',style='yellow bold')
             time.sleep(.5)
             level+=1
             console.print(f' Advance to Level {level}',style='green italic')
             time.sleep(.5)
-
             break
         else:
             wrong_answer_count+=1
